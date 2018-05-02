bea076e3b940c5fb823896e86ee7e6dc4802a596
Tim Klein <timjklein36@gmail.com>
2018-05-01 20:20:17 -0400

    Add cloud-formation reference files

--- models/EC2.py
+++ models/EC2.py
@@ -1,11 +1,11 @@
 from peewee import *
-from playhouse.fields import ManyToManyField
 
 db = SqliteDatabase('cloud-compile.db')
 
+
 class EC2(Model):
     id = CharField()
-    groupSet = ManyToManyField(ScurityGroup, backref='ec2s')
+    
 
 
     class Meta:
