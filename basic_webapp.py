36be8f813e8af831f4699551395e484d379c1c89
Matthew Roll <mproll@pm.me>
2018-05-20 17:01:35 -0400

    abstract a bunch of stuff into a util file. support elastic ip
    modified:   basic_webapp.py
    new file:   util.py

--- basic_webapp.py
+++ basic_webapp.py
@@ -1,9 +1,5 @@
-import boto3
-from botocore.exceptions import ClientError
-
-ec2_client = boto3.client('ec2')
-ec2_resource = boto3.resource('ec2')
-
+from util import (associate_elastic_ip__ec2, create_ec2, create_keypair,
+                  create_security_group, create_vpc, get_vpc_subnet)
 
 """
 ----------------------------------------------------------------------
@@ -22,27 +18,12 @@ uses existing:
   - dhcp options
 ----------------------------------------------------------------------
 """
-vpc = ec2_resource.create_vpc(CidrBlock='10.0.0.0/16')
-vpc.wait_until_available()
-print(vpc.id)
 
-subnet = vpc.create_subnet(CidrBlock='10.0.0.1/24')
-
-create_ig_response = ec2_client.create_internet_gateway()
-ig = ec2_resource.InternetGateway(
-    create_ig_response["InternetGateway"]["InternetGatewayId"]
-)
-
-vpc.attach_internet_gateway(InternetGatewayId=ig.id)
-print(ig.id)
-
-
-route_table = list(vpc.route_tables.all())[0]
-route_table.associate_with_subnet(SubnetId=subnet.id)
-route_table.create_route(
-    DestinationCidrBlock='0.0.0.0/0',
-    GatewayId=ig.id,
-)
+subnets = [
+    {'CidrBlock': '10.0.1.0/24', 'Public': True}
+]
+vpc = create_vpc('10.0.0.0/16', subnets)
+print('Created vpc {}'.format(vpc.id))
 
 
 """
@@ -58,41 +39,32 @@ creates:
   - network interface
 
 """
-# Create sec group
-sec_group_response = ec2_client.create_security_group(
-    GroupName='webapp',
-    Description='basic webapp sec group',
-    VpcId=vpc.id
-)
-sec_group = ec2_resource.SecurityGroup(sec_group_response['GroupId'])
 
-sec_group.authorize_ingress(
-    CidrIp='0.0.0.0/0',
-    IpProtocol='tcp',
-    FromPort=22,
-    ToPort=22
+security_group = create_security_group(
+    'webapp',
+    vpc.id,
+    IngressRules=[
+        {
+            'CidrIp': '0.0.0.0/0',
+            'IpProtocol': 'tcp',
+            'FromPort': 22,
+            'ToPort': 22
+        }
+    ]
 )
+print('Created security group {}'.format(security_group.id))
 
-try:
-    key_pair = ec2_client.create_key_pair(
-        KeyName='basic-webapp-key',
-    )
-    print(key_pair['KeyMaterial'])
-except ClientError:
-    print('Key pair basic-web-app already exists')
 
-instances = ec2_resource.create_instances(
-    ImageId='ami-916f59f4',
-    MinCount=1,
-    MaxCount=1,
-    KeyName='basic-webapp-key',
-    InstanceType='t2.micro',
-    NetworkInterfaces=[{
-        'SubnetId': subnet.id,
-        'DeviceIndex': 0,
-        'AssociatePublicIpAddress': True,
-        'Groups': [sec_group.group_id]}
-    ],
-)
-instances[0].wait_until_running()
-print(instances[0].id)
+KeyName = 'basic-webapp-key'
+create_keypair(KeyName)
+
+subnet = get_vpc_subnet(vpc, '10.0.1.0/24')
+
+instance = create_ec2('ami-916f59f4', KeyName, subnet.id, security_group.id)
+elastic_ip = associate_elastic_ip__ec2(instance.id)
+
+print('Created EC2 instance {}'.format(instance.id))
+print('Created Elastic IP {} and pointed it at {}'.format(
+    elastic_ip,
+    instance.id
+))
---
+++ util.py
@@ -0,0 +1,165 @@
+import boto3
+from botocore.exceptions import ClientError
+
+ec2_client = boto3.client('ec2')
+ec2_resource = boto3.resource('ec2')
+
+
+def first(f, l):
+    for el in l:
+        if f(el):
+            return el
+
+
+def get_vpc_subnet(vpc, cidr_block):
+    global ec2_resource
+
+    def filt(subnet):
+        return subnet.vpc == vpc and subnet.cidr_block == cidr_block
+
+    return first(filt, ec2_resource.subnets.all())
+
+
+def get_vpc_route_table(vpc):
+    return list(vpc.route_tables.all())[0]
+
+
+def associate_vpc__internet_gateway(vpc):
+    global ec2_client
+
+    create_ig_response = ec2_client.create_internet_gateway()
+    ig = ec2_resource.InternetGateway(
+        create_ig_response["InternetGateway"]["InternetGatewayId"]
+    )
+
+    vpc.attach_internet_gateway(InternetGatewayId=ig.id)
+    return ig
+
+
+def create_vpc(
+        CidrBlock,
+        SubnetSpecs=[],
+        AttachInternetGateway=True
+):
+    global ec2_resource
+
+    vpc = ec2_resource.create_vpc(CidrBlock=CidrBlock)
+    vpc.wait_until_available()
+
+    if AttachInternetGateway:
+        ig = associate_vpc__internet_gateway(vpc)
+
+    route_table = get_vpc_route_table(vpc)
+    for subnet_spec in SubnetSpecs:
+        subnet = vpc.create_subnet(CidrBlock=subnet_spec['CidrBlock'])
+
+        if AttachInternetGateway and subnet_spec['Public']:
+            route_table.associate_with_subnet(SubnetId=subnet.id)
+            route_table.create_route(
+                DestinationCidrBlock='0.0.0.0/0',
+                GatewayId=ig.id,
+            )
+
+    return vpc
+
+
+def associate_elastic_ip__ec2(ec2id):
+    global ec2_client
+
+    public_ip = ''
+    allocation = ''
+    elastic_ips = ec2_client.describe_addresses()
+
+    for address in elastic_ips['Addresses']:
+        print('Found existing Elastic IP: ', address)
+        if 'InstanceId' not in address:
+            allocation = address['AllocationId']
+            public_ip = address['PublicIp']
+            break
+
+    if allocation == '':
+        try:
+            allocation_response = ec2_client.allocate_address(Domain='vpc')
+            allocation = allocation_response['AllocationId']
+            public_ip = allocation_response['PublicIp']
+        except ClientError as e:
+            print(e)
+
+    try:
+        ec2_client.associate_address(
+            AllocationId=allocation,
+            InstanceId=ec2id,
+        )
+    except ClientError as e:
+        print(e)
+
+    return public_ip
+
+
+def create_security_group(
+        GroupName,
+        VpcId,
+        Description='default description',
+        IngressRules=[],
+        EgressRules=[]
+):
+    global ec2_client, ec2_resource
+
+    security_group_response = ec2_client.create_security_group(
+        GroupName=GroupName,
+        Description=Description,
+        VpcId=VpcId
+    )
+    security_group = ec2_resource.SecurityGroup(security_group_response['GroupId'])
+
+    for ingress_rule in IngressRules:
+        security_group.authorize_ingress(**ingress_rule)
+
+    for egress_rule in EgressRules:
+        security_group.authorize_egress(**ingress_rule)
+
+    return security_group
+
+
+def create_keypair(KeyName, outfile=None):
+    try:
+        key_pair = ec2_client.create_key_pair(
+            KeyName=KeyName,
+        )
+        if outfile:
+            with open(outfile, 'w') as fp:
+                fp.write(key_pair['KeyMaterial'])
+        else:
+            print(key_pair['KeyMaterial'])
+    except ClientError as e:
+        print('Found keypair {}'.format(KeyName))
+
+    return KeyName
+
+
+def create_ec2(
+        Ami, KeyName, SubnetId, SecurityGroupId,
+        InstanceType='t2.micro',
+        MinCount=1,
+        MaxCount=1
+):
+    global ec2_resource
+
+    instances = ec2_resource.create_instances(
+        ImageId=Ami,
+        MinCount=MinCount,
+        MaxCount=MaxCount,
+        KeyName=KeyName,
+        InstanceType=InstanceType,
+        NetworkInterfaces=[{
+            'SubnetId': SubnetId,
+            'DeviceIndex': 0,
+            'AssociatePublicIpAddress': True,
+            'Groups': [SecurityGroupId]}
+        ],
+    )
+    instance = instances[0]
+
+    instance.wait_until_running()
+
+    return(instance)
